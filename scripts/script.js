1b10aba12ed53f829123f4b2737ded1fe0dc1414
Elvira Kiamova <ela.kiyamova@yandex.ru>
2020-09-01 13:13:05 +0500

    исправления по всему проекту

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
-let popup = document.querySelector(".popup"); // нашла сам попап
-let popupOpen = document.querySelector(".profile__rectangle"); // нашла кнопку открытие попапа
-let popupClose = document.querySelector(".popup__icon"); // нашла крестик (в форме) для закрытия попапа
-
-console.log({ popup, popupOpen, popupClose }); // проверила правьность подключения файла JS
+let popup = document.querySelector(".popup");
+let popupOpen = document.querySelector(".profile__rectangle"); // кнопка с карандашом
+let popupClose = document.querySelector(".popup__icon-click"); // крестик закрытия формы
+let inputName = document.querySelector(".popup__input-name"); // инпут имя
+let inputAboutme = document.querySelector(".popup__input-aboutme"); // инпут после имени
+let formElement = document.querySelector(".popup__container"); // форма
+let popupButton = document.querySelector(".popup__button"); // кнопка сохранить
+let title = document.querySelector(".profile__title"); // заколовок
+let subtitle = document.querySelector(".profile__subtitle"); // параграф
 
 let popupToggle = function () {
   popup.classList.toggle("popup_opened");
@@ -11,36 +15,25 @@ let popupToggle = function () {
 popupOpen.addEventListener("click", popupToggle); // вызов функции при клике по карандашу для добавления класса
 popupClose.addEventListener("click", popupToggle); // вызов функции при клике на крестик для удаления класса
 
-// Находим форму в DOM
-let formElement = document.querySelector(".popup__container");
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
-  // Находим все поля формы в DOM
-  let inputs = document.querySelectorAll("input");
-
-  // Получаем значение полей из свойства value
-  console.log(inputs[0].value);
-  console.log(inputs[1].value);
-
-  // Выбераем элементы, куда должны быть вставлены значения полей
-  let title = document.querySelector("h1");
-  let subtitle = document.querySelector("p");
-
   // Вставляем новые значения с помощью textContent
-  title.textContent = inputs[0].value;
-  subtitle.textContent = inputs[1].value;
+  title.textContent = inputName.value;
+  subtitle.textContent = inputAboutme.value;
+  popupToggle();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
 
-let popupButton = document.querySelector(".popup__button"); // нашла кнопку "Сохранить" в форме
+function ClickPopap() {
+  inputName.value = title.innerHTML;
+  inputAboutme.value = subtitle.innerHTML;
+} // функция сохранения данных со странички в инпуты формы при каждом открытии попапа
 
-popupButton.addEventListener("click", popupToggle); // вызов функции при клике по "Сохранить" для удаления класса в попап
+ClickPopap("click", popupToggle);
