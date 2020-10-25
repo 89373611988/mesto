2c790f0e800f654a022dfb4dd8b21bb21d15eb57
Elvira Kiamova <ela.kiyamova@yandex.ru>
2020-10-25 17:21:05 +0500

    добавление template, удаление карточки, клик по сердцу

--- scripts/script.js
+++ scripts/script.js
@@ -41,3 +41,84 @@ function ClickPopap() {
 }
 popupOpen.addEventListener("click", ClickPopap); // вызов функции при клике по карандашу для добавления класса
 
+// ---------------------------------------------------------------------------------------------- добавление template, удаление карточки, клик по сердцу
+
+const initialCards = [
+  {
+    name: "Уфа",
+    link: "./images/ufa.jpg",
+  },
+  {
+    name: "Крым",
+    link: "./images/crimea.jpg",
+  },
+  {
+    name: "Домбай",
+    link: "./images/Dombai.png",
+  },
+  {
+    name: "Гора Эльбрус",
+    link: "./images/e.png",
+  },
+  {
+    name: "Москва",
+    link: "./images/Moscow.jpg",
+  },
+  {
+    name: "Карачаево-Черкесия",
+    link: "./images/karachay.png",
+  },
+];
+
+// const popupButton = popup.querySelector(".popup__button"); // кнопка Создать новую карточку
+const inputNameCard = popup.querySelector(".popup__input_denomination"); // инпут Название (в форме сохранить карточку)
+const inputLinkCard = popup.querySelector(".popup__input_link"); // инпут Ссылки (в форме сохранить карточку)
+const elementsCard = document.querySelector("#elements").content; // получаем содержимое template тега
+const CardSection = document.querySelector(".elements"); // секция с карточками куда добавляются клоны темплейт
+const listElement = document.querySelector("element"); // выбрали элемент который нужно удалить
+const deleteButton = document.querySelector(".element__urn"); // кнопка удаления
+
+CardSection.addEventListener('click', function(e) {
+    if (e.target.classList.contains('element__vector')){
+      e.target.classList.toggle('element__vector_dark')
+    }
+});
+
+// функция добавления template на страницу
+function renderItem(card, index) {
+  const userElement = elementsCard.cloneNode(true); // клонируем содержимое тега template
+  userElement.querySelector(".element__image").src = card.link; // наполняем содержимым ссылку на картинку
+  userElement.querySelector(".element__title").innerText = card.name; // наполняем содержимым название картинки
+  userElement.querySelector(".element__image").alt = card.name; // наполняем содержимым alt
+  userElement.querySelector(".element").setAttribute("id", index); // присваеваем id элементу, чтобы потом удалить его из массива
+  CardSection.appendChild(userElement); // отображаем на странице
+
+  // функиця добавления новой карточки
+  function cardSubmit(evt) {
+    evt.preventDefault();
+    initialCards.unshift(renderItem); // добавляем методом unshift карточку в начало массива
+    render();
+  }
+  popupButton.addEventListener("submit", cardSubmit);
+}
+
+function render() {
+  CardSection.innerHTML = ""; // обнуляем, чтобы карточки не копировались
+  initialCards.forEach(renderItem);
+  setListeners();
+}
+
+
+function handleCardDelete(event) {
+  const index = event.target.parentNode.getAttribute("id"); // находим элемент в массиве который нужно удалить по id
+  initialCards.splice(index, 1); // из массива вырезаем один элемент с данным индексом
+  render();
+}
+
+function setListeners() {
+  document.querySelectorAll(".element__urn").forEach((btn) => {
+   btn.addEventListener("click", handleCardDelete);
+  });
+}
+
+render();
